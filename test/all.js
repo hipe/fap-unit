cb06a5b88367698f468db4771dcb3cccd2a2a059
Mark Meves <mark.meves@gmail.com>
2011-03-20 01:49:04 -0400

    stupid simple little test case that is also pedagogic

---
+++ test/all.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+;
+
+require.paths.unshift(__dirname + '/../lib'); // make below prettier
+
+// start story "example usage"
+
+// here's some code you want to test
+function addTwo(a, b) { return a + b; }
+
+// require the module
+var fapunit = require('fap-unit');
+
+// make a test case object, choose a name
+var tc = fapunit.testCase('My Widget');
+
+// add a test function to the test case, give it a name
+tc.test('add should work', function(){
+
+  var res = addTwo(1, 2);
+
+  this.assert.equal(3, res, "result should be 3");
+
+});
+
+tc.run(); // for now we run this thing explicitly
+
+// end story
+
