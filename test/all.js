3ec8d7b56ae923b3e8b15b469f08ebe1d799d205
Mark Meves <mark.meves@gmail.com>
2011-03-23 04:42:24 -0400

    Here we have a test that runs another test inside of it, and captures the output from it, and asserts that output against a target

--- test/all.js
+++ test/all.js
@@ -1,29 +1,52 @@
 #!/usr/bin/env node
 ;
 
-require.paths.unshift(__dirname + '/../lib'); // make below prettier
 
-// start story "example usage"
+var fu = require('../lib/fap-unit');
+var all = fu.testCase('all');
 
-// here's some code you want to test
-function addTwo(a, b) { return a + b; }
 
-// require the module
-var fapunit = require('fap-unit');
 
-// make a test case object, choose a name
-var tc = fapunit.testCase('My Widget');
+all.test('basic usage', function() {
 
-// add a test function to the test case, give it a name
-tc.test('add should work', function(){
+  require.paths.unshift(__dirname + '/../lib'); // make below prettier
 
-  var res = addTwo(1, 2);
+  this.captureOutput(function(){
 
-  this.assert.equal(3, res, "result should be 3");
+    // start story "basic usage"
 
+    // here's some code you want to test
+    function addTwo(a, b) { return a + b; }
+
+    // require the module
+    var fapunit = require('fap-unit');
+
+    // make a test case object, choose a name
+    var tc = fapunit.testCase('My Widget');
+
+    // add a test function to the test case, give it a name
+    tc.test('add should work', function(){
+
+      var res = addTwo(1, 2);
+
+      this.assert.equal(3, res, "result should be 3");
+
+    });
+
+    tc.run(); // for now we run this thing explicitly
+
+    // end story
+
+  });
+
+  var tgt = 'Loaded case My Widget\n'+
+    'Started\n'+
+    '.\n'+
+    'Finished in 0 seconds.\n'+
+    'ohai in red\n1 tests, 1 assertions, 0 failures, 0 errors\n';
+  var have = fu.uncolorize(this.lastOutput);
+  this.assert.equal(tgt, have);
 });
 
-tc.run(); // for now we run this thing explicitly
 
-// end story
-
+all.run();
